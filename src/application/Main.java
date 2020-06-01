37582bcf33695a96eb29efb33af32be0f8bd1740
vechniymuzikant <yaroslav.valentiy@gmail.com>
2020-06-01 10:49:14 +0300

    Added gsmes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,18 @@
 package application;
 
 import java.nio.file.Paths;
+import java.util.HashMap;
 
 import javafx.application.Application;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import view.ViewManager;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 
 public class Main extends Application {
+	public static HashMap<KeyCode,Boolean> keys = new HashMap<>();
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -28,6 +29,7 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	public static boolean isPressed(KeyCode key) {return keys.getOrDefault(key,false);}
 
 	MediaPlayer mediaPlayer;
 	public void music(){
--- src/view/GameViewManager.java
+++ src/view/GameViewManager.java
@@ -1,5 +1,7 @@
 package view;
 
+import application.Main;
+import javafx.animation.AnimationTimer;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,59 +13,104 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class GameViewManager {
 
-	private AnchorPane gamePane;
+	public static AnchorPane gamePane;
 	private Scene gameScene;
 	private Stage gameStage;
 	private Stage menuStage;
-	private final static int GAME_HEIGHT = 600;
+	public static ArrayList<Block> blocks = new ArrayList<>();
+	private final static int GAME_HEIGHT = 800;
 	private final static int GAME_WIDTH = 800;
-
-
+	public final static int BLOCK_SIZE = 40;
+	public final static int CHARACTER_SIZE = 35;
+	private int levelWidth;
+	private int levelNumber = 0;
+	private PacMan player;
 	public GameViewManager() throws IOException {
 		initializeStage();
-		createKeyListeners();
 	}
-
-	private void createKeyListeners() {
-		gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-    		@Override
-    		public void handle(KeyEvent event) {
-    		if(event.getCode()==KeyCode.LEFT) {
-
-    		}else if(event.getCode()==KeyCode.RIGHT) {
-
-    		}
-    		}
-    	});
-
-		gameScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-    		@Override
-    		public void handle(KeyEvent event) {
-    		if(event.getCode()==KeyCode.LEFT) {
-
-    		}else if(event.getCode()==KeyCode.RIGHT) {
-
-    		}
-    		}
-    	});
-
-	}
-
 	private void initializeStage() throws IOException {
 		gamePane = new AnchorPane();
-		gamePane = FXMLLoader.load(getClass()
-                .getResource("/application/level1new.fxml"));
 		gamePane.setStyle("-fx-background-color: transparent; -fx-background-image: url('/view/resources/fon2.gif');");
-
 		gameScene = new Scene(gamePane, GAME_WIDTH, GAME_HEIGHT);
 		gameStage = new Stage();
+		gameScene.setOnKeyPressed(event-> Main.keys.put(event.getCode(), true));
+		gameScene.setOnKeyReleased(event -> {
+			Main.keys.put(event.getCode(), false);
+		});
+		levelWidth = model.LevelData.levels[levelNumber][0].length()*BLOCK_SIZE;
+		for(int i = 0; i < model.LevelData.levels[levelNumber].length; i++){
+			String line = model.LevelData.levels[levelNumber][i];
+			for(int j = 0; j < line.length(); j++){
+				switch (line.charAt(j)){
+					case '0':
+						break;
+					case '1':
+						Block platformFloor = new Block(Block.BlockType.PLATFORM, j * BLOCK_SIZE, i * BLOCK_SIZE);
+						break;
+					case '2':
+						Block brick = new Block(Block.BlockType.BRICK,j*BLOCK_SIZE,i*BLOCK_SIZE);
+						break;
+					case '3':
+						Block bonus = new Block(Block.BlockType.BONUS,j*BLOCK_SIZE,i*BLOCK_SIZE);
+						break;
+					case '4':
+						Block stone = new Block(Block.BlockType.STONE,j * BLOCK_SIZE, i * BLOCK_SIZE);
+						break;
+					case '5':
+						Block PipeTopBlock = new Block(Block.BlockType.PIPE_TOP,j * BLOCK_SIZE, i * BLOCK_SIZE);
+						break;
+					case '6':
+						Block PipeBottomBlock = new Block(Block.BlockType.PIPE_BOTTOM,j * BLOCK_SIZE, i * BLOCK_SIZE);
+						break;
+					case '*':
+						Block InvisibleBlock = new Block(Block.BlockType.INVISIBLE_BLOCK,j * BLOCK_SIZE, i * BLOCK_SIZE);
+						break;
+				}
+			}
+
+		}
+		player = new PacMan();
+		player.setTranslateX(160);
+		player.setTranslateY(760);
+		player.translateXProperty().addListener((obs,old,newValue)->{
+			int offset = newValue.intValue();
+			if(offset>640 && offset<levelWidth-640){
+				gamePane.setLayoutX(-(offset-640));
+			}
+		});
+		gamePane.getChildren().add(player);
 		gameStage.setScene(gameScene);
 		gameStage.setResizable(false);
+		AnimationTimer timer = new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				update();
+			}
+		};
+		timer.start();
 	}
-
+	private void update(){
+		if(Main.isPressed(KeyCode.UP) && player.getTranslateY()>=5){
+			player.setRotate(-90);
+			player.moveY(-5);
+		}
+		if(Main.isPressed(KeyCode.LEFT) && player.getTranslateX()>=5){
+			player.setRotate(180);
+			player.moveX(-5);
+		}
+		if(Main.isPressed(KeyCode.RIGHT) && player.getTranslateX()+40 <=levelWidth-5){
+			player.setRotate(0);
+			player.moveX(5);
+		}
+		if(Main.isPressed(KeyCode.DOWN)) {
+			player.setRotate(90);
+			player.moveY(5);
+		}
+	}
 	public void createNewGame(Stage menuStage) {
 		this.menuStage = menuStage;
 		this.menuStage.hide();
