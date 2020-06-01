a6ca1dff663c3f9ce132487eeecb0758ade2b5ec
YelyzavetaAndriienko <65223500+YelyzavetaAndriienko@users.noreply.github.com>
2020-06-01 12:32:02 +0300

    Add files via upload

---
+++ src/model/ScoreLabel.java
@@ -0,0 +1,23 @@
+package model;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class ScoreLabel extends Label{
+
+	public ScoreLabel(String text) {
+		setPrefWidth(400);
+		setPrefHeight(400);
+		setPadding(new Insets(40,40,40,40));
+		setText(text);
+		setWrapText(true);
+		setLabelFont();
+		setTextFill(Color.WHITE);
+	}
+	
+	public void setLabelFont() {
+		setFont(Font.font("Constantia", 40));
+	}
+}
