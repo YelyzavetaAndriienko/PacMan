ef160ca2a5026ceba26e24504aec8379a86fa223
YelyzavetaAndriienko <65223500+YelyzavetaAndriienko@users.noreply.github.com>
2020-06-08 11:43:56 +0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,15 +2,14 @@ package application;
 
 import java.nio.file.Paths;
 import java.util.HashMap;
-
 import javafx.application.Application;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import view.ViewManager;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-
-
+/**
+ * class: Main
+ * Implements the program
+ */
 public class Main extends Application {
 	public static HashMap<KeyCode,Boolean> keys = new HashMap<>();
 	@Override
@@ -25,9 +24,20 @@ public class Main extends Application {
 		}
 	}
 
+/**
+ * main
+ * @param args
+ */
 	public static void main(String[] args) {
 		launch(args);
 	}
-	public static boolean isPressed(KeyCode key) {return keys.getOrDefault(key,false);}
-
+	
+/**
+ * isPressed
+ * @param key
+ * @return
+ */
+	public static boolean isPressed(KeyCode key) {
+		return keys.getOrDefault(key,false);
+		}
 }
