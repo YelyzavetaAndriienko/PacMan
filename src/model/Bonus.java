dc42f8b5f1239c3122bec96dc4cf3c9d7258b30e
YelyzavetaAndriienko <65223500+YelyzavetaAndriienko@users.noreply.github.com>
2020-06-08 11:44:29 +0300

    Update Bonus.java

--- src/model/Bonus.java
+++ src/model/Bonus.java
@@ -1,38 +1,34 @@
 package model;
 
 import java.nio.file.Paths;
-
-import javafx.geometry.Point2D;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.util.Duration;
 import view.GameViewManager;
-import view.Block.BlockType;
-
+/**
+ * class: Bonus
+ * Describes behavior of the bonus
+ */
 public class Bonus extends Pane {
 	 Image bonusImage;
 	 ImageView bonusView;
 	 public static MediaPlayer mediaPlayerBonusShield;
 	 public static MediaPlayer mediaPlayerBonusHeart;
-//	 ImageView bonusStar = new ImageView("model/resources/b2.gif");
-//	 ImageView bonusShield = new ImageView("model/resources/b3.png");
-//	 public final static int HEART_HP = 10;
+	 
 	 public enum BonusType {
 		 HEART, SHIELD, STAR
 	 }
 	public MyAnimation animation;
 	public BonusType type;
-//	    public Point2D playerVelocity = new Point2D(0,0);
-//	 protected ImageView bonus;
-//	 public enum BonusType{
-//		 HEART, SHIELD, STAR
-//	 }
-	    
+	  
+/**
+ * constructor
+ * @param typeB
+ * @param x
+ * @param y
+ */
 	 public Bonus (BonusType typeB, int x, int y) {
 		 type = typeB;
 		 switch (type) {
@@ -55,11 +51,18 @@ public class Bonus extends Pane {
 	     GameViewManager.bonuses.add(this);
 	     GameViewManager.gamePane.getChildren().add(this);
 	 }
-	 
+
+/**
+ * removes bonuses from the panel
+ */
 	 public void removeBonus() {
 		 GameViewManager.gamePane.getChildren().remove(this);
 	 }
-	 
+
+/**
+ * choose the bonus
+ * @param l
+ */
 	 public void action(ScoreLabel l) {
 		 switch (type) {
          case HEART:
@@ -74,6 +77,9 @@ public class Bonus extends Pane {
 		 }
 	 }
 
+/**
+ * action of a shield
+ */
     private void shieldAction() {
     	if(GameViewManager.hasShield == false) {
     		GameViewManager.hasShield = true;
@@ -83,42 +89,41 @@ public class Bonus extends Pane {
     	}
 	}
 
-//	    public Bonus(ImageView b){
-//	      bonus = b;
-//	      bonus.setFitHeight(GameViewManager.BONUS_SIZE);
-//	      bonus.setFitWidth(GameViewManager.BONUS_SIZE);
-//	      getChildren().addAll(bonus);
-//	      bonusStar.setFitHeight(50);
-//	      bonusStar.setFitWidth(40);
-//	    //  getChildren().addAll(bonusStar);
-//	      bonusShield.setFitHeight(50);
-//	      bonusShield.setFitWidth(40);
-//	     // getChildren().addAll(bonusShield);
-//	    }
-
+/**
+* action of a shield
+*/
 	    private void heartAction(ScoreLabel l){
 	    	int score = Integer.parseInt(l.getText());
 	    	int newScore = 0;
-	    	if(score<=90) {
-		    	newScore = score + 10;
+	    	if(score<80) {
+		    	newScore = score + 20;
 		    	String textScore = String.valueOf(newScore);
 		        l.setText(textScore);
 		        musicBonusHeart();
 		        removeBonus();
+	    	}else{
+		        musicBonusHeart();
+		        removeBonus();
 	    	}
 	    }
-	    
+	   
+/**
+ * add music for heart
+ */
 		public void musicBonusHeart(){
-		   // String bip = "C:\\Users\\Liza\\Downloads\\bonusSound2.mp3";
-			String bip = "src/view/resources/bonusSound2.mp3";
+		   String bip = "C:\\Users\\Liza\\Downloads\\bonusSound2.mp3";
+			//String bip = "src/view/resources/bonusSound2.mp3";
 		    Media hit = new Media(Paths.get(bip).toUri().toString());
 		    mediaPlayerBonusHeart = new MediaPlayer(hit);
 		    mediaPlayerBonusHeart.play();
 		}
 		
-			public void musicBonusShield(){
-			  //  String bip = "C:\\Users\\Liza\\Downloads\\bonusSound1.mp3";
-				String bip = "src/view/resources/bonusSound1.mp3";
+/**
+ * adds music for shield	
+ */
+		public void musicBonusShield(){
+			    String bip = "C:\\Users\\Liza\\Downloads\\bonusSound1.mp3";
+				//String bip = "src/view/resources/bonusSound1.mp3";
 			    Media hit = new Media(Paths.get(bip).toUri().toString());
 			    mediaPlayerBonusShield = new MediaPlayer(hit);
 			  mediaPlayerBonusShield.play();
